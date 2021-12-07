d9cfed673516b3787783562c97e82ca92b772957
Leon Si <contact@leonzalion.com>
2021-12-06 22:12:33 -0500

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	useTabs: true,
+	singleQuote: true,
+	pugSingleFileComponentIndentation: false,
+	pugSingleQuote: false,
+	pugAttributeSeparator: "always",
+	pugWrapAttributesPattern: "1",
+	pugEmptyAttributes: "none"
+}
